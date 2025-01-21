a0edab4b3559da889a429a591a525ee6eeb7256d
ilgiz091 <ilgiz091@mail.ru>
2025-01-21 20:50:22 +0300

    Add files via upload

--- module_16_1.py
+++ module_16_1.py
@@ -4,16 +4,16 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return ("Главная страница")
+    return "Главная страница"
 
 @app.get("/user/admin")
 async def admin():
-    return ("Вы вошли как администратор")
+    return "Вы вошли как администратор"
 
 @app.get("/user/{user_id}")
 async def get_user(user_id: int):
-    return (f'Вы вошли как пользователь № {user_id}')
+    return f'Вы вошли как пользователь № {user_id}'
 
 @app.get("/user/")
 async def get_user_age(username: str, age: int):
-    return (f'Информация о пользователе. Имя: {username}, Возраст: {age}')
+    return f'Информация о пользователе. Имя: {username}, Возраст: {age}'
